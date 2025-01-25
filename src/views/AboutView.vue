6401ac7652f924d3346e03f5511314aadb51dc5e
waseem abbas <waseemofficee@gmail.com>
2025-01-25 21:07:06 +0500

    ref in template

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,18 +1,29 @@
 <template>
-    <div class="text-center">
-      <div>
-        <h4>{{ counterData.counterTitle }}:</h4>
+<div class="text-center">
+    <div>
+        <h4 ref="appTitle">{{ counterData.counterTitle }}:</h4>
         <input class="form-control" v-model="counterData.counterTitle" type="text" placeholder="Edit Title" v-autofocus>
     </div>
-    </div>
-    </template>
-    <script setup>
-    import { vAutofocus } from '@/directives/autoFocus';
-   import { reactive } from 'vue';
-    const counterData = reactive({
+</div>
+</template>
+
+    
+<script setup>
+import {
+    vAutofocus
+} from '@/directives/autoFocus';
+import { watch } from 'vue';
+import {
+    reactive,
+    ref
+} from 'vue';
+const counterData = reactive({
     counter: 0,
     counterTitle: "Input autofocus",
-    
+});
+const appTitle = ref(null);
+watch(() => appTitle.value?.offsetWidth, (newValue, oldValue) => {
+  console.log(oldValue);
+  console.log(newValue);
 })
-    </script>
-    
+</script>
---
+++ src/views/tinyTextEditor.vue
@@ -0,0 +1,12 @@
+<template>
+         <textarea class="form-control h-p80"  name="tinymce" id="tinymceExample" rows="10"></textarea>
+             
+</template>
+
+<script setup>
+  
+</script >
+
+<style lang="scss" scoped>
+
+</style>
