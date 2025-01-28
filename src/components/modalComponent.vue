6d126b4ddebd590ca5412dc06d8235c5833ab32f
waseem abbas <waseemofficee@gmail.com>
2025-01-28 15:02:51 +0500

    import modal

---
+++ src/components/modalComponent.vue
@@ -0,0 +1,40 @@
+<template>
+<teleport to="body">
+    <div class="modal-overlay" @click.self="hideModal">
+        <div class="text-dark modal-content bg-gradient rounded shadow m-4 p-4">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, ea minus nesciunt sequi dignissimos beatae cumque debitis facilis iure atque ratione soluta impedit optio mollitia laudantium, accusamus fugit.</p>
+            <button @click="hideModal" class="btn btn-primary mt-2">Hide modal</button>
+        </div>
+    </div>
+</teleport>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+
+.modal-content {
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+  max-width: 500px;
+  text-align: center;
+}
+</style>
