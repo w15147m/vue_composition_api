ccfb9584aecbff9f62cfeed378c4bc8fdb5a5e3d
waseem abbas <waseemofficee@gmail.com>
2025-01-28 15:17:55 +0500

    props

--- src/components/modalComponent.vue
+++ src/components/modalComponent.vue
@@ -1,35 +1,43 @@
 <template>
 <teleport to="body">
- <slot></slot>
+    <div class="modal-overlay" @click.self="hideModal">
+        <div class="text-dark modal-content bg-gradient rounded shadow m-4 p-4">
+            <h2>{{ title }}</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, ea minus nesciunt sequi dignissimos beatae cumque debitis facilis iure atque ratione soluta impedit optio mollitia laudantium, accusamus fugit.</p>
+            <button @click="hideModal" class="btn btn-primary mt-2">Hide modal</button>
+        </div>
+    </div>
 </teleport>
 </template>
 
-<script>
-export default {
+<script setup>
+const props = defineProps({
+    title: String,
+    default: "there is no title"
+  
+})
 
-}
 </script>
-
 
 <style>
 .modal-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 1000;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1000;
 }
 
 .modal-content {
-  background: white;
-  padding: 20px;
-  border-radius: 8px;
-  max-width: 500px;
-  text-align: center;
+    background: white;
+    padding: 20px;
+    border-radius: 8px;
+    max-width: 500px;
+    text-align: center;
 }
 </style>
--- src/views/posts/modalView.vue
+++ src/views/posts/modalView.vue
@@ -3,13 +3,8 @@
     <h1 class="text-center mt-4">Modals</h1>
     <button @click="showModal" class="btn btn-primary mt-2">Show modal</button>
     <!-- Teleport the modal content to the body -->
-    <modalComponent >
-      <div class="modal-overlay" v-if="isShow" @click.self="hideModal">
-        <div class="text-dark modal-content bg-gradient rounded shadow m-4 p-4">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, ea minus nesciunt sequi dignissimos beatae cumque debitis facilis iure atque ratione soluta impedit optio mollitia laudantium, accusamus fugit.</p>
-            <button @click="hideModal" class="btn btn-primary mt-2">Hide modal</button>
-        </div>
-    </div>
+    <modalComponent v-if="isShow"  :title="title">
+      
     </modalComponent>
   </div>
 </template>
@@ -17,7 +12,7 @@
 <script setup>
 import { ref } from "vue";
 import modalComponent from "@/components/modalComponent.vue";
-
+const title = ref("My title");
 const isShow = ref(false); // Default state is hidden
 const showModal = () => {
   isShow.value = true;
